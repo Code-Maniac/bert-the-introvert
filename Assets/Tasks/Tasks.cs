6986c33132285226f98f70fd0f3709abd1fba297
profhatchet <profhatchet@gmail.com>
2023-09-30 23:18:57 +0100

    Started Creating Dynamic Job Lists

--- Assets/Tasks/Tasks.cs
+++ Assets/Tasks/Tasks.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class Tasks : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     GameObject nextTask;
     int jobIndex;
     int taskIndex;
+    bool jobsComplete = false;
 
 
 
-    static String[] job1 = { "Fridge", "Treadmill" };
+    static String[] job1 = { "Fridge", "Treadmill", "Bin" };
     static String[] job2 = { "Fridge", "Treadmill" };
     String[][] jobs = { job1, job2 };
 
@@ -30,20 +30,94 @@ public class Tasks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (nextTask.GetComponent<Proximity>().inUse)
+        if (!jobsComplete && nextTask.GetComponent<Proximity>().inUse)
         {
             Debug.Log($"{jobs[jobIndex][taskIndex]} JOB COMPLETE");
             taskIndex += 1;
             if (taskIndex >= jobs[jobIndex].Length)
             {
                 Debug.Log("TASK COMPLETE!!");
-                Debug.Log($"taskIndex: {taskIndex}");
-                Debug.Log($"jobIndex: {jobIndex}");
                 jobIndex += 1;
                 taskIndex = 0;
             }
-            Debug.Log($"Setting Task {jobs[jobIndex][taskIndex]}");
-            nextTask = GameObject.FindWithTag(jobs[jobIndex][taskIndex]);
+            if (jobIndex < jobs.Length)
+            {
+                Debug.Log($"Setting Task {jobs[jobIndex][taskIndex]}");
+                nextTask = GameObject.FindWithTag(jobs[jobIndex][taskIndex]);
+            }
+            else
+            {
+                Debug.Log("ALL JOBS COMPLETE!!");
+                jobsComplete = true;
+            }
+
         }
     }
 }
+
+
+
+
+
+
+
+// public class Tasks : MonoBehaviour
+// {
+//     [System.Serializable]
+//     public struct Jobs
+//     {
+//         public GameObject taskName;
+//         public int jobNumber;
+//     }
+//     public Jobs[] jobStruct;
+
+//     GameObject nextTask;
+//     int jobIndex;
+//     int taskIndex;
+//     bool jobsComplete = false;
+
+
+
+
+//     static String[] job1 = { "Fridge", "Treadmill", "Bin" };
+//     static String[] job2 = { "Fridge", "Treadmill" };
+//     String[][] jobs = { job1, job2 };
+
+
+//     void Start()
+//     {
+//         Debug.Log(jobStruct[0]);
+
+
+//         taskIndex = 0;
+//         jobIndex = 0;
+//         Debug.Log($"Setting Task {jobs[jobIndex][taskIndex]}");
+//         nextTask = GameObject.FindWithTag(jobs[jobIndex][taskIndex]);
+//     }
+
+//     void Update()
+//     {
+//         if (!jobsComplete && nextTask.GetComponent<Proximity>().inUse)
+//         {
+//             Debug.Log($"{jobs[jobIndex][taskIndex]} JOB COMPLETE");
+//             taskIndex += 1;
+//             if (taskIndex >= jobs[jobIndex].Length)
+//             {
+//                 Debug.Log("TASK COMPLETE!!");
+//                 jobIndex += 1;
+//                 taskIndex = 0;
+//             }
+//             if (jobIndex < jobs.Length)
+//             {
+//                 Debug.Log($"Setting Task {jobs[jobIndex][taskIndex]}");
+//                 nextTask = GameObject.FindWithTag(jobs[jobIndex][taskIndex]);
+//             }
+//             else
+//             {
+//                 Debug.Log("ALL JOBS COMPLETE!!");
+//                 jobsComplete = true;
+//             }
+
+//         }
+//     }
+// }
